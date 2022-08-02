da94421de116361c58b8c7dd1d4ed56e757f2e68
lizayao <lizayao95@hotmail.com>
2022-08-01 22:44:21 -0300

    final

--- javascript/bolsa.js
+++ javascript/bolsa.js
@@ -12,101 +12,80 @@ const totalBolsa = () => {
 }
 
 const body = document.getElementById('bolsa');
-if(bolsa.length == 0){
-    const texto = `
-        <div class='contenedorBolsa'>
-            <h1 class='textoBolsa'>No ha seleccionado ningún producto</h1>
-            <a class='btnVolver' href='../index.html'>
-                <button>Volver</button>
-            </a>
-        </div>`;    
-    body.innerHTML += texto;
-} else {
-    const titulo = `
-        <div class='contenedorBolsa'>
-            <h1 class='textoBolsa'>Bolsa de compras</h1>
-            <a class='btnVolver' href='../index.html'>
-                <button>Seguir eligiendo</button>
-            </a>
-        </div>`;
-    body.innerHTML += titulo;
-    const tabla = `
-        <div class='contenedorTabla'>
-            <table>
-                <thead>
-                    <tr>
-                        <th class='txtTabla'>Producto</th>
-                        <th class='txtTabla'>Cantidad</th>
-                        <th class='txtTabla'>Precio</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody id='tbody'>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th class='txtTotal'>Total:</th>
-                        <th id='total'>$${totalBolsa().toLocaleString()}</th>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-        <div class='btnContenedor'>
-            <button class='btnComprar'><a href="compra.html">Comprar</a></button>
-        </div>
-        <div class='btnContenedor'>
-            <button id='vaciarBolsa'>Vaciar bolsa</button>
-         </div>
-        `;
-    body.innerHTML += tabla;
-    const tbody = document.getElementById('tbody')
+function mostrarBolsa(){
+    body.innerHTML = '';
+    if(bolsa.length == 0){
+        const texto = `
+            <div class='contenedorBolsa'>
+                <h1 class='textoBolsa'>No ha seleccionado ningún producto</h1>
+                <a class='btnVolver' href='../index.html'>
+                    <button>Volver</button>
+                </a>
+            </div>`;    
+        body.innerHTML += texto;
+    } else {
+        const titulo = `
+            <div class='contenedorBolsa'>
+                <h1 class='textoBolsa'>Bolsa de compras</h1>
+                <a class='btnVolver' href='../index.html'>
+                    <button>Seguir eligiendo</button>
+                </a>
+            </div>`;
+        body.innerHTML += titulo;
+        const tabla = `
+            <div class='contenedorTabla'>
+                <table>
+                    <thead>
+                        <tr>
+                            <th class='txtTabla'>Producto</th>
+                            <th class='txtTabla'>Cantidad</th>
+                            <th class='txtTabla'>Precio</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody id='tbody'>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th></th>
+                            <th></th>
+                            <th class='txtTotal'>Total:</th>
+                            <th id='total'>$${totalBolsa().toLocaleString()}</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+            <div class='btnContenedor'>
+                <button class='btnComprar'><a href="compra.html">Comprar</a></button>
+            </div>
+            <div class='btnContenedor'>
+                <button id='vaciarBolsa'>Vaciar bolsa</button>
+            </div>
+            `;
+        body.innerHTML += tabla;
+        const tbody = document.getElementById('tbody')
 
-    for (let i = 0; i < bolsa.length; i++) {
-        const element = bolsa[i];
-        const {id, nombre, img, precio, cantidad} = element
+        for (let i = 0; i < bolsa.length; i++) {
+            const element = bolsa[i];
+            const {id, nombre, img, precio, cantidad} = element
 
-        const resumen = `
-            <tr id=${id}>
-                <th class='detallesTabla'><img class='imgProducto' src=${img} alt='foto del producto'></th>
-                <th class='tituloProd'>${nombre}</th>
-                <th>${cantidad}</th>
-                <th>$${(cantidad * precio).toLocaleString()}</th>    
-            </tr>`
-        tbody.innerHTML += resumen
+            const resumen = `
+                <tr id=${id}>
+                    <th class='detallesTabla'><img class='imgProducto' src=${img} alt='foto del producto'></th>
+                    <th class='tituloProd'>${nombre}</th>
+                    <th>${cantidad}</th>
+                    <th>$${(cantidad * precio).toLocaleString()}</th>    
+                </tr>`
+            tbody.innerHTML += resumen
+        }
     }
-}  
-
-//VACIAR BOLSA //
-let vaciarBolsa = document.getElementById(vaciarBolsa)
-vaciarBolsa.onclick = () => {
-    bolsa = []
+    let vaciarBolsa = document.getElementById('vaciarBolsa')
+    vaciarBolsa.onclick = () => {
+    bolsa = [];
+    localStorage.setItem("bolsa", bolsa);
+    mostrarBolsa();
+}
 }
 
-
-// ELIMINAR PRODUCTO //
-
-/* function eliminarProducto(id, bolsa){
-    Swal.fire({
-        title: '¿Desea eliminar este producto?',
-        text: "Podrá volver a agregarlo luego",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Eliminar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            bolsa = bolsa.filter( e => e.id !== id);
-            localStorage.setItem("bolsa", bolsa);
-            actualizarBolsa();
-            Swal.fire(
-            'Producto eliminado',
-            'Recuerde que puede volver a agregarlo',
-            'success'
-            )
-        }
-    })
-} */
+mostrarBolsa();
